b224b02649ad20a8ed26784d974dd5daf0495684
Atsuya Kumano <at.kumano@gmail.com>
2018-09-01 21:02:52 -0700

    metar data cleaning logic

--- utils/datautils.py
+++ utils/datautils.py
@@ -1,6 +1,7 @@
-import re
 import pandas as pd
 from utils import geoutils
+import re
+from datetime import datetime
 
 # e.g. csv='../data/yellow_tripdata_2016-01_small.csv'
 def read_rides(csv):
@@ -41,3 +42,29 @@ def clean_rides(df):
     in_nyc = df[['pickup_latitude','pickup_longitude']].apply(
             lambda row: geoutils.is_in_nyc(*row), axis=1)
     return df[in_nyc]
+
+def read_metar(csv):
+    usecols = ['valid', 'tmpf', ' p01i'] # p01i has a whitespace in its name
+    df = pd.read_csv(csv, usecols=usecols)
+    df.columns = ['datetime', 'fahrenheit', 'precip_in']
+    df['datetime'] = pd.to_datetime(df['datetime'])
+
+    # precipitation and temperature each has its own processing logic; need to work on them separately.
+    precip = df[['datetime','precip_in']]
+    # Precip info comes at the 51st minute of each hour.
+    # Ensure there is one precip record for each hour.
+    assert sum(precip['datetime'].dt.minute == 51) == 24
+    precip = precip[precip['datetime'].dt.minute == 51]
+    # Drop the minute information so the datetime format matches that of fahrenheit_avg.
+    precip['datetime'] = pd.to_datetime(precip['datetime'].dt.strftime("%Y-%m-%d %H"))
+
+    # Take the average of temperature records in each hour.
+    fahrenheit = df[['datetime','fahrenheit']]
+    # drop the minute information so records in the same hour are put in the same group.
+    fahrenheit['datetime'] = pd.to_datetime(fahrenheit['datetime'].dt.strftime("%Y-%m-%d %H"))
+    fahrenheit_avg = fahrenheit.groupby(['datetime']).mean()
+
+    # inner join 
+    weather = pd.merge(precip, fahrenheit_avg, on='datetime', how='inner')
+    assert weather.shape[0] == 24, "Something is wrong at the join. There isn't one record for each hour."
+    return weather.set_index('datetime')
